ab52c18c919ea541a6718e266c53499072a84a82
Harsh22428 <harsh.22428@knit.ac.in>
2025-09-05 12:46:04 +0530

    added modal effect in cart page

--- app/(tabs)/menu/_layout.tsx
+++ app/(tabs)/menu/_layout.tsx
@@ -1,10 +1,32 @@
+import  FontAwesome  from "@expo/vector-icons/FontAwesome";
 import { Stack } from "expo-router";
-
-export default function MenuStack(){
-    return (
-        <Stack>
-            <Stack.Screen name="index" options={{title:"Menu",headerTitleAlign:"center"}}/>
-           
-        </Stack>
-    )
+import  Colors  from "@/constants/Colors";
+import { Link } from "expo-router";
+import { Pressable } from "react-native";
+export default function MenuStack() {
+  return (
+    <Stack
+      screenOptions={{
+        headerRight: () => (
+          <Link href="/cart" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <FontAwesome
+                  name="shopping-cart"
+                  size={25}
+                  color={Colors.light.tint}
+                  style={{ marginRight: 10, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+        )
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{ title: "Menu", headerTitleAlign: "center" }}
+      />
+    </Stack>
+  );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,8 +14,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
+      <Stack >
+        <Stack.Screen name="(tabs)"  options={{ headerShown: false }}/>
+        <Stack.Screen name="cart" options={{presentation:"modal",animation:"slide_from_bottom"}}/>
       </Stack>
     </ThemeProvider>
   );
