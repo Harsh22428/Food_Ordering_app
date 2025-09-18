6a939599d44e251fd5f965ec1bed6a7089dbd78f
Harsh22428 <harsh.22428@knit.ac.in>
2025-09-18 16:32:12 +0530

    added orders pages of users and admin

--- types/types.ts
+++ types/types.ts
@@ -6,8 +6,25 @@ export type Product={
 };
 
 export type PizzaSize='S'|'M'|'L'|'XL';
+export type OrderStatus = 'New' | 'Cooking' | 'Delivering' | 'Delivered';
 
+export type Order = {
+  id: number;
+  created_at: string;
+  total: number;
+  user_id: string;
+  status: OrderStatus;
 
+  order_items?: OrderItem[];
+};
+export type OrderItem = {
+  id: number;
+  product_id: number;
+  products: Product;
+  order_id: number;
+  size: PizzaSize;
+  quantity: number;
+};
 export type CartItem={
     id:string;
     product:Product;
